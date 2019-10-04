0e74f2ed0c0728a919dbddd18b61f75c9feec437
Spenser Black <spenserblack01@gmail.com>
2019-10-04 14:10:58 -0400

    Start project

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
+pub trait LargestUnit {
+    fn largest_unit(&self, precision: u8, unit: &str) -> String;
+    fn largest_byte_unit(&self, precision: u8) -> String;
+}
+
+impl LargestUnit for f32 {
+    fn largest_unit(&self, precision: u8, unit: &str) -> String {
+        String::new()
+    }
+
+    fn largest_byte_unit(&self, precision: u8) -> String {
+        String::new()
+    }
+}
+impl LargestUnit for f64 {
+    fn largest_unit(&self, precision: u8, unit: &str) -> String {
+        String::new()
+    }
+
+    fn largest_byte_unit(&self, precision: u8) -> String {
+        String::new()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn kilometer_test() {
+        assert_eq!(1_129.0.largest_unit(2, "m"), "1.13 km");
+    }
+
+    #[test]
+    fn gibibyte_test() {
+        assert_eq!(2_635_000_987.0.largest_byte_unit(1), "2.6 GiB");
     }
 }
